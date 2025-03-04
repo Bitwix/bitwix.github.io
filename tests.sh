4e0e87090d383984f68dbe8b19989dc99fa555fe
Jo Clarke <jo@bitwix.com>
2025-03-05 11:40:56 +1300

    committed cycletracker code

--- tests.sh
+++
@@ -1,9 +0,0 @@
-cat passmaker.js > xxxx.js
-cat tester.js >> xxxx.js
-
-node xxxx.js "You've got a fast car I want a ticket to anywhere"
-node xxxx.js "Don't you know I still believe That you will be here, and give me a sign?"
-node xxxx.js "she was just 17 if you know what I mean"
-node xxxx.js "let it go, let it go, I'm one with the wind and the sky"
-node xxxx.js "wake me up before you gogo I'm not planning on going solo"
-
